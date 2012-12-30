1b84be72fb1c47ed35e3142878ae99214b14e17f
fregas <me@craigscode.com>
2012-12-29 22:11:21 -0600

    other additions

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,13 @@
 
 $(function() {
+  $('#submit_button').attr('disabled', 'disabled');
+
   $(".answer a").click(function(e) {
     var answer_id = $(e.currentTarget).attr('answer_id');
     $('#invitee_answer_id').val(answer_id);
     $('div.answer-item').css('border-color','transparent');
     $(e.currentTarget).parent().css('border-color','black');
+    $('#submit_button').removeAttr('disabled');
 
     return false;  
   });
